777adffeff55c84311e0faedde1d783d001218e2
Pieter Claus <pieter@MacBook-Pro-van-Pieter.local>
2023-05-25 16:20:01 +0200

    finished project

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,18 @@ import List from './assets/List.jsx';
 const App = () => {
   const [people, setPeople] = useState(peopleList);
 
+  const handleClick = () => {
+    setPeople([]);
+  };
+
   return (
     <>
       <section className='container'>
         <h3>{people.length} birthdays today</h3>
         <List people={people} />
+        <button type='button' className='btn' onClick={handleClick}>
+          Reset
+        </button>
       </section>
     </>
   );
